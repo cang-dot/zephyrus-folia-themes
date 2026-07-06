6a5b5d06df759347c3d6f1f20ddb52621bc16e42
cang-dot <WEISTUDIOWEI@126.com>
2026-07-06 15:43:14 +0800

    chore: remove temp check script

--- check.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const dist = path.join(__dirname, 'dist')
-
-const c = fs.readFileSync(path.join(dist, 'folia-fume.js'), 'utf8')
-const idx = c.indexOf('function(m,k)')
-const before = c.substring(Math.max(0, idx - 30), idx)
-console.log('before:', JSON.stringify(before))
